bcf4d9b19f1f00178f36bbc027dc652f866a8838
PhaelLima-j <raphael.rodrigues.58010@a.fecaf.com.br>
2024-11-21 14:02:49 -0300

    Estruturando as pastas

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+const createError = require('http-errors');
+const express = require('express');
+const passport = require('passport');
+
+const { logger } = require('./utils');
+const { connect } = require('./models');
+const router = require('./routes');
+
+const app = express();
+
+//  configurando autenticação
+app.use(passport.initialize());
+
+// configurando formatos de parâmetros
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// declarando rotas
+app.use('/', router);
+
+// caso nenhuma rota de match, redireciona para a 404
+app.use(function(_req, _res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, _req, res, _next) {
+  res.status(err.status || 500);
+  res.json({
+    sucesso: false,
+    erro: err.message,
+  });
+});
+
+const porta = 3000;
+app.listen(porta, () => {
+  connect();
+
+  logger.info(`Servidor ouvindo na porta ${porta}`);
+});
+
+module.exports = app;

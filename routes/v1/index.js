4a1a3b17255086053349f29019aa80dec2140155
Raphael Lima <rapha.lima.rl16@gmail.com>
2024-11-26 21:00:21 -0300

    Rota de autenticação

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const { checaAutenticado } = require('./middlewares/checa-autenticacao');
+
 
 const statusRouter = require('./status');
 const usuariosRouter = require('./usuarios');
+const authRouter = require('./auth');
 
 const router = express.Router();
 
 router.use('/status', statusRouter);
 router.use('/usuarios', usuariosRouter);
+router.use('/auth', authRouter);
 
 
 module.exports = router;
